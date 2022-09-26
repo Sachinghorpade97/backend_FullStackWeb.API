0ac83bd219fc410519c91b1dd2ed34ec1a93d719
Sachinghorpade97 <ghorpadesachin382@gmail.com>
2022-09-26 10:15:27 +0530

    Add project files.

---
+++ FullStackWeb.API/Data/FullStackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FullStackWeb.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackWeb.API.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ FullStackWeb.API/Program.cs
@@ -0,0 +1,34 @@
+using FullStackWeb.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<FullStackDbContext>(options => options.UseSqlServer(connection));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
